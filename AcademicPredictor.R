4aca89f2d1cacfc42ae7af4e0f31fdb0c40ee9b0
tomaslopez99 <tomy.lopez1999@gmail.com>
2019-05-24 14:43:45 -0300

    update

--- AcademicPredictor.R
+++ AcademicPredictor.R
@@ -1,5 +1,6 @@
 #Insert data
 library(readxl)
+library(dplyr)
 getwd()
 ##admissions18_19 <- read_excel("/Users/Katy/Desktop/MuyNecesario/Predictor/Ingreso18-19.xlsx")
 ##entry19 <- read_excel("/Users/Katy/Desktop/MuyNecesario/Predictor/Ingreso19.xlsx")
@@ -8,11 +9,11 @@ admissions18_19 <- read_excel("Admissions18-19.xlsx")
 entry19 <- read_excel("Ingreso19.xlsx")
 entry18 <- read_excel("Ingreso18.xlsx")
 
+
 #Defining colnames of the three data frames
 names(entry18) <- c("ID", "Status", "Name", "Sex", "Career", "Cohort", "Entry", "Cal.Math", "Rec.Math", "Cal.Physics", "Rec.Physics", "IC.Average", "BenefitType", "RequestedBenefit", "GetScholarship", "SchoolAverage", "PercentageGiven", "School")
 names(entry19) <- c("ID", "Status", "Name", "Sex", "Career", "Cohort", "Entry", "Cal.Math", "Rec.Math", "Cal.Physics", "Rec.Physics", "IC.Average", "BenefitType", "RequestedBenefit", "GetScholarship", "SchoolAverage", "PercentageGiven", "ApplicantStatus", "EngineeringStatus", "School")
 names(admissions18_19) <- c("ID", "Entry", "Name", "Sex", "School", "Nationality", "Province", "Area", "GraduationYear", "Career", "EntryCourse", "Cal.Physics", "Cal.Math", "Scholarship", "SchoolAverage", "RecultOfEC")
-
 
 #MODIFYING ENTRY19
 #Merging engineering status with applicant status
@@ -33,6 +34,7 @@ entry18_19$GetScholarship <- mapvalues(entry18_19$GetScholarship, obtieneBecaPhr
 #Deleting all requested benefit that were not obtained
 entry18_19$RequestedBenefit[which(entry18_19$GetScholarship=="NO")] <- NA
 
+
 names(entry18_19)
 entry18_19$GetScholarship <- NULL #Delete get scholarship because the negatives were clean up in requested benefit
 entry18_19$BenefitType <- NULL #Delete benefit type becuase it is not relevant for our study
@@ -41,14 +43,12 @@ entry18_19 <- rename(entry18_19, replace = c("RequestedBenefit" = "Scholarship")
 entry18_19$Scholarship[which(!is.na(entry18_19$PercentageGiven))] <- entry18_19$PercentageGiven[which(!is.na(entry18_19$PercentageGiven))]
 entry18_19$PercentageGiven <- NULL #Delete percentage given because the info was replace in scholarship
 
-#ACA ME QUEDE
 
-#Suming the ones with two types of Scholarship
-unique(entry18_19$Scholarship)
-becaPhrases <- c("40/ 20","40/20","20/20")
-becaMeanings <- c("60","60","40")
-entry18_19$Scholarship <- mapvalues(entry18_19$Scholarship, becaPhrases, becaMeanings)
-#Correcting the data of Scholarship to show only the number
+unique(entry18_19$Beca)
+scholarshipPhrases <- c("40/ 20","40/20","20/20")
+scholarshipMeanings <- c("60","60","40")
+entry18_19$Scholarship <- mapvalues(entry18_19$Scholarship, scholarshipPhrases, scholarshipMeanings)
+#Correcting the data of Beca to show only the number
 entry18_19$Scholarship[grepl("100",entry18_19$Scholarship)] <- "100" 
 entry18_19$Scholarship[!grepl("100",entry18_19$Scholarship)] <- substr(entry18_19$Scholarship[!grepl("100",entry18_19$Scholarship)], 1, 2)
 
@@ -60,10 +60,8 @@ entry18_19$Status <- mapvalues(entry18_19$Status, matriculadoPhrases, rep("Matri
 bajaPhrases <- c(entry18_19$Status[grep("baja",entry18_19$Status,TRUE)],"FEBRERO",NA)
 entry18_19$Status <- mapvalues(entry18_19$Status, bajaPhrases, rep("Baja", length(bajaPhrases)))
 
-
 admissions18_19$Province[which(admissions18_19$ID=="272")] <- NA
 
-#Correcting schools
 admissions18_19[admissions18_19 == "Michael Ham (Nordelta)"] <- "Michael Ham"
 admissions18_19[admissions18_19 == "Michael Ham (Vicente López)"] <- "Michael Ham"
 admissions18_19[admissions18_19 == "Lincoln (La Lucila)"] <- "Lincoln"
@@ -75,19 +73,18 @@ admissions18_19[admissions18_19 == "Pilgrims´ (San Isidro)"] <- "Pilgrims´"
 admissions18_19[admissions18_19 == "St. Mary of the Hills (San Fernando)"] <- "St. Mary of the Hills"
 admissions18_19[admissions18_19 == "St. Mary of the Hills (Pilar)"] <- "St. Mary of the Hills"
 
-#Correcting cals and recs
+
 delete <- c("AUS", "ausente", "-", "A", "APROBADO", "Desaprobado", "Es pase", "x", "Es pase interno")
+
 entry18_19$Cal.Math <- mapvalues(entry18_19$Cal.Math, delete, rep(NA, length(delete)))
 entry18_19$Rec.Math <- mapvalues(entry18_19$Rec.Math, delete, rep(NA, length(delete)))
 entry18_19$Cal.Physics <- mapvalues(entry18_19$Cal.Physics, delete, rep(NA, length(delete)))
 entry18_19$Rec.Physics <- mapvalues(entry18_19$Rec.Physics, delete, rep(NA, length(delete)))
 
-#Modifying format to cals and recs
 entry18_19$Cal.Math = as.numeric(entry18_19$Cal.Math)
 entry18_19$Rec.Math = as.numeric(entry18_19$Rec.Math)
 entry18_19$Cal.Physics = as.numeric(entry18_19$Cal.Physics)
 entry18_19$Rec.Physics = as.numeric(entry18_19$Rec.Physics)
-
 
 entry18_19$IC.Average <- round(rowMeans(entry18_19[,8:11], na.rm = TRUE), digits = 2)
 entry18_19[entry18_19 == "NaN"] <- NA
@@ -119,3 +116,4 @@ df$IC.Average[df$IC.Average == "NaN"] <- NA
 
 finalEntry <- rbind(entry18_19, df)
 finalEntry
+
